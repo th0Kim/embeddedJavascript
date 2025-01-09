1677d29252584f19f458bf68bfc7f16de44d78f0
kimsoyoung <3001th0@naver.com>
2025-01-09 14:21:50 +0900

    주석 수정

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 // server.js
-const express = require("express");
-const app = express();
-const path = require("path");
+const express = require("express"); // express 모듈을 사용하기 위한 express 객체 생성
+const path = require("path"); // 경로 설정을 위한 path 모듈
+
+const app = express(); // express 모듈을 사용하기 위한 express 객체 생성
 
 // 정적 파일 제공
 app.use(express.static(path.join(__dirname, "/public")));
 
 // EJS 설정
 app.set("view engine", "ejs");
-app.set("views", __dirname + "/views"); // views 폴더 설정
+app.set("views", __dirname + "/views");
 
 // 라우팅
 app.get("/", (req, res) => {
-  res.render("index"); // index.ejs 파일을 렌더링
+  res.render("index");
 });
 
 // 서버 실행
