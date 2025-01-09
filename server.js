3f7f11412b0a88232bf97c9ffdd7d2863d28aeac
kimsoyoung <3001th0@naver.com>
2025-01-09 14:25:27 +0900

    node-sass-middleware 추가하며 scss 변경이 있을 때 자동으로 css 컴파일 해주도록 설정

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 // server.js
 const express = require("express"); // express 모듈을 사용하기 위한 express 객체 생성
 const path = require("path"); // 경로 설정을 위한 path 모듈
+const sassMiddleware = require("node-sass-middleware"); // sass 파일을 css 파일로 변환하기 위한 node-sass-middleware 모듈
 
 const app = express(); // express 모듈을 사용하기 위한 express 객체 생성
+
+// Sass 미들웨어 설정
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, "public/scss"), // SCSS 파일 경로
+    dest: path.join(__dirname, "public/css"), // CSS 파일 출력 경로
+    debug: true, // 디버그 로그 활성화
+    outputStyle: "compressed", // CSS 출력 형식: nested, expanded, compact, compressed
+    prefix: "/css", // URL 경로에서 /css로 제공
+  })
+);
 
 // 정적 파일 제공
 app.use(express.static(path.join(__dirname, "/public")));
